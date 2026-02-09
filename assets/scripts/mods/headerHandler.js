5b4e8351a718c47ee9dcb656cec44ff6c4436ec8
projectnum17 <egor.chornyy@gmail.com>
2026-02-09 15:48:19 +0200

    add header scrolled

--- assets/scripts/mods/headerHandler.js
+++ assets/scripts/mods/headerHandler.js
@@ -17,6 +17,35 @@ const headerHandler = () => {
         });
     };
 
+    const headerScrolledHandler = () => {
+        const header = document.querySelector('.js-header');
+        if (!header) return;
+
+        let lastScroll = 0;
+
+        const handleScroll = () => {
+            const currentScroll = window.scrollY;
+
+            if (currentScroll > 10) {
+                header.classList.add('is-scroll');
+            } else {
+                header.classList.remove('is-scroll');
+            }
+
+            if (currentScroll > lastScroll && currentScroll > 100) {
+                header.classList.add('is-transform');
+            } else {
+                header.classList.remove('is-transform');
+            }
+
+            lastScroll = currentScroll;
+        };
+
+        handleScroll();
+        window.addEventListener('scroll', handleScroll);
+    };
+
+    headerScrolledHandler();
     targetBtnsHandler();
 };
 
